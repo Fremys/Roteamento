d07d9f14436be8a984db91ec3f02587183aa823c
Matheus Aguilar <matheusfremys@gmail.com>
2024-11-30 00:47:05 -0300

    ADD calculation of economic AND a small  code rafactory

--- src/main.py
+++ src/main.py
@@ -2,52 +2,111 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
+import sys
 
 class Route:
-    graph = nx.cubical_graph()
+    graph = nx.null_graph
+    centerNode = ""
             
-    def __init__(self, graph):
+    def __init__(self, graph, centerNode):
         self.graph = graph
+        self.centerNode=centerNode
     
-    def printGraph(self):
+    def print_graph(self):
         nx.draw(self.graph, with_labels=True, pos=nx.circular_layout(self.graph), node_color='r', edge_color='b', font_weight='bold')
+        # Obtendo os pesos das arestas
+        edge_labels = nx.get_edge_attributes(self.graph, 'weight')  # Extrai os pesos das arestas
+        nx.draw_networkx_edge_labels(self.graph, nx.circular_layout(self.graph), edge_labels=edge_labels, font_color='red')
         plt.show()
-    
+             
+    def print_edges_info(self, origin, dest):
+        """_summary_
 
-def build_example_route():
+        CUIDADO!
+        ESSE MÉTODO É DE USO INTERNO DA CLASSE
+        
+        Esse metodo mostra na tela a aresta e seu
+        respectivo peso
+        
+        Args:
+            origin (string): nó de origem da aresta
+            dest (string): nó de destino da aresta
+        """
+        if(self.graph.has_edge(origin, dest)):
+            print(f"Aresta: {origin} - {dest} | Valor = {self.graph.get_edge_data(origin, dest).get("weight")}") 
+        else:
+            print(f"Aresta: {origin} - {dest} | Valor = N/A")     
+            
+    def print_edges_and_weight(self):
+        
+        nodes = list(self.graph.nodes) #resgatar lista de nos do grafo
+        
+        for origin in nodes:
+            for dest in nodes:
+                if(origin != dest):
+                    self.print_edges_info(origin, dest)
+    
+        
+    
+def build_example_route(centerNode="D"):
     #definir dados
     exampleGraph = nx.Graph() 
     listNode = ["A", "B", "C", "D", "E", "F"]
-    listEdge = build_edges_with_center_node(listNode, "D")
+    listEdge = build_edges_with_center_node(listNode, centerNode)
     
     exampleGraph.add_nodes_from(listNode) #adicionando os nos ao grafo
     exampleGraph.add_weighted_edges_from(listEdge) #adicionando as arestas ao grafo 
     
     return exampleGraph
-    
+
+def add_edge_to_nodes(originNode, destNode, weight, centerNode, listEdgeResult):
+    if(originNode == centerNode):
+        listEdgeResult.append((originNode, destNode, weight)) #construir rota do central para todos os outros no da rede
+    else:
+        if(random.choice([True, False])):
+            listEdgeResult.append((originNode, destNode, weight)) #construir as outras rotas da rede
     
 def build_edges_with_center_node(listNode, centerNode):
     
     listEdgeResult = []
     
-    for nodeOrigin in listNode:
-        for nodeDest in listNode:
+    for originNode in listNode:
+        for destNode in listNode:
             weight = random.randint(1, 20)
-            if(nodeDest != nodeOrigin): #nao construir rotas para o mesmo lugar
-                if(nodeOrigin == centerNode):
-                    listEdgeResult.append((nodeOrigin, nodeDest, weight)) #construir rota do central para todos os outros no da rede
-                else:
-                    if(random.choice([True, False])):
-                        listEdgeResult.append((nodeOrigin, nodeDest, weight)) #construir as outras rotas da rede
+            if(destNode != originNode): #nao construir loops no grafo
+                add_edge_to_nodes(originNode, destNode, weight, centerNode, listEdgeResult)
                     
     return listEdgeResult
 
+def calc_economic(firstNode, secondNode, centerNode, graph):
+    
+    if(not(graph.has_edge(firstNode, secondNode))):
+        return (-1) * sys.maxsize
+    
+    firstToOriginD =  get_edge_value(graph, centerNode, firstNode)
+    secondToOriginD = get_edge_value(graph, centerNode, secondNode)
+    firstToSecondD =  get_edge_value(graph, firstNode, secondNode)
+        
+    return firstToOriginD + secondToOriginD - firstToSecondD 
+
+def get_edge_value(graph, originNode, destNode):
+    return graph.get_edge_data(originNode, destNode).get("weight")
 
 def main():
     
-    graph = Route(build_example_route())
-    graph.printGraph()
+    route = Route(build_example_route("D"), "D")
+    # route.graph.
+    # route = Route(nx.graph_atlas(), 3)
+    listNodes = list(route.graph.nodes)
     
-    print("ola mundo")
+    # route.graph.get_edge_data(listNodes[0], listNodes[1]).get("weight")
+    # print(listNodes)
+    dist = calc_economic(listNodes[0], listNodes[1], route.centerNode, route.graph)
+    print(f"distancia : {dist}")
+    
+    route.print_edges_and_weight()
+    
+    route.print_graph()
 
+#Executar funcao principal
 main()
